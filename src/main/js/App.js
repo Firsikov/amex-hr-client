81ee403cc033c0dbbe8fdeb1d852d98c8c22169d
Firsikov <Alex_Parol>
2019-03-10 17:24:15 +0000

    fetching of reports hierarchy

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -1,35 +1,67 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import '../css/App.css';
 
 export default class App extends Component {
 
     state = {
-        data: {}
+        data: {},
     };
 
-    inputChange = event => {
-        this.setState({
-            searchString: event.target.value
+    componentDidMount() {
+        this.initialize()
+    }
+
+    initialize = async () => {
+        const data = await this.transform(1);
+        this.setState({ data });
+    };
+
+    transform = id =>
+        new Promise(async resolve => {
+            const employee = await this.fetchEmployeeById(id)
+                .then(this.processServerReponse)
+                .then(this.processJson(id))
+                .catch(console.error);
+
+            if(!employee) return;
+
+            const reports = _.map(employee.reports,
+                async reportId => await this.transform(reportId));
+
+            Promise.all(reports)
+                .then(this.transformReports(employee))
+                .then(resolve);
         });
+
+    transformReports = ({ name, id }) => reports =>
+        ({ [name]: { id, reports } });
+
+    processServerReponse = response => {
+        if(response.ok)
+            return response.text();
+
+        return Promise.reject(`Failed to fetch data from the server: Error code ${response.status}`)
     };
 
-    search = () => {
-        fetch(`/employee/${this.state.searchString}`).then(result => result.json()).then(data => this.setState({ data }));
-    };
+    processJson = employeeId => response => {
+        if(_.isEmpty(response))
+            return Promise.reject(`Employee with id ${employeeId} not found`);
 
-    keyDown = event => {
-        if(event.keyCode === 13) {
-            this.search();
+        try {
+            return JSON.parse(response);
+        } catch (e) {
+            return Promise.reject(`Failed to parse response from the server for id ${employeeId}`);
         }
     };
+
+    fetchEmployeeById = id => fetch(`/employee/${id}`);
 
     render() {
         const data = JSON.stringify(this.state.data);
         return (
             <div className="App">
                 <header className="App-header">
-                    <input onChange={this.inputChange} onKeyDown={this.keyDown} />
-                    <button onClick={this.search}>Search!</button>
                     <p>
                         { data }
                     </p>
